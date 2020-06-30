2775aa6bd31289130f00f2499510a11aa7b89e61
Ivan N Paz <ivannpaz@gmail.com>
2020-06-30 08:56:42 +0200

    Update controller

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage() : Response
     {
         return new Response("Here we are");
+    }
+
+    /**
+     * @Route("/question/{slug}")
+     */
+    public function question($slug) : Response
+    {
+        return new Response(sprintf("Question reply: %s", $slug));
     }
 }
